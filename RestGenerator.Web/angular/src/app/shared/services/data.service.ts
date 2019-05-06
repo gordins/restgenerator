0fc413cf9ce72f94849a4b1802f4f93bdf26708f
gordins <stefangordindev@gmail.com>
2019-05-06 19:16:47 +0300

    frontend adjustments

--- RestGenerator.Web/angular/src/app/shared/services/data.service.ts
+++ RestGenerator.Web/angular/src/app/shared/services/data.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs/operators';
 import { Api } from '../models';
+
+const apiUrl: string = '/api/api';
+const resourceUrl: string = '/api/resource';
+const fieldUrl: string = '/api/field';
 
 @Injectable()
 export class DataService {
-    api: BehaviorSubject<Api> = new BehaviorSubject<Api>(new Api());
-    changeApi(newApi: Api): void {
-        this.api.next(newApi);
+    apis: Api[] = null;
+
+    constructor(private readonly http: HttpClient) { }
+
+    getAllApis(): Api[] {
+        if (this.apis === null) {
+            this.http.get<Api[]>(apiUrl).pipe(take(1)).subscribe((apis: Api[]) => {
+                this.apis = apis;
+            });
+        }
+        return this.apis;
     }
-    //resource: BehaviorSubject<Resource> = new BehaviorSubject<Resource>(new Resource());
-    //changeResource(newResource: Resource): void {
-    //  this.resource.next(newResource);
-    //}
+
 }
